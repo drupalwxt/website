512cc41ed4c9271de9a7c5bce49cdfa1145fd403
William Hearn <sylus1984@gmail.com>
2015-07-28 14:51:08 -0400

    Adjust logic.

--- dev.sh
+++ dev.sh
@@ -3,6 +3,7 @@
 echo -e "\033[0;32mStarting Hugo...\033[0m"
 
 # Build the project.
-bower install
-cd themes/wxt && bower install && cd ../..
+cd themes
+git clone https://github.com/drupalwxt/wet-boew-hugo.git wxt
+cd wxt && bower install && cd ../..
 hugo server --theme=wxt --buildDrafts --watch
